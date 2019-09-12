60b3e594c5016f391cae5f201f63bacabbcd3b04
saurabh <saurabhthakre165@gmail.com>
2019-09-12 10:43:28 +0530

    make it better

--- server.js
+++ server.js
@@ -14,3 +14,5 @@ res.sendFile(path.join(__dirname,'/dist/scheduleapp/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log('Console Listening!');
