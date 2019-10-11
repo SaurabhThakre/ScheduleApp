ceb538f4fb9d890dad06db41bea47437e4cc2be3
unknown <saurabhthakre165@gmail.com>
2019-10-11 17:16:37 +0530

    deploy commit1

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/scheduleapp'));
+app.use(express.static(__dirname + './dist/scheduleapp'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/scheduleapp/index.html'));
+res.sendFile(path.join(__dirname + '/dist/scheduleapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-console.log('Console Listening!');
+app.listen(process.env.PORT || 8000);
